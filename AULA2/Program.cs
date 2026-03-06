097e9ea0abd4ef791d4c3fa0b13dd0eb9562cba7
OtavioAugustodaRoza <otavioaugustodaroza@gmail.com>
2026-03-06 16:16:07 -0300

    conclusão aula 2

---
+++ AULA2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AULA2 //forma de organização 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CFB CURSOS");
+            if(args.GetLength(0)>0){
+            Console.Write(args.GetValue(0));
+            }
+        }
+    }
+}
