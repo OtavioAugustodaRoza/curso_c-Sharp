2b95c5ef609767006d4d3d128a376ed57b76c8f7
OtavioAugustodaRoza <otavioaugustodaroza@gmail.com>
2026-03-07 10:34:30 -0300

    FEAT: termino aula 5

---
+++ AULA5/aula05.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Aula5
+{
+    static void Main()
+    {
+      //  int res=(10+5) * 2;
+
+      // bool res = 10>5; // operadores relacionais >, <, >=, <=, !=, ==
+
+     // int num = 10;
+    //  num += 1;  operador de soma 
+    //  num -= 1;  operador de subtração 
+    //  num++;  
+    //  num--;  
+
+    //operadores logicos 
+    // | / or / ou
+    // & / and / E
+
+     //   bool res = (10<44) & (20>100);
+          bool res = (10<44) | (20>100);
+
+        Console.WriteLine(res);
+    }
+}
