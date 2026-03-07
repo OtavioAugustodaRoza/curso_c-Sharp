d13d789eebb00d1fb5a15804c2f7b37c686dc6f3
OtavioAugustodaRoza <otavioaugustodaroza@gmail.com>
2026-03-07 14:45:24 -0300

    FEAT: AULA 10 enumeradores

---
+++ AULA10/aula10.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Aula10
+{  
+    enum DiasSemana{Domingo,Segunda,Terça,Quarta,Quinta,Sexta,sabado};
+    static void Main()
+    {
+     //   DiasSemana ds = DiasSemana.Domingo;
+     //   DiasSemana ds = (DiasSemana)3; //pegando quarta pelo indice
+       int ds = (int)DiasSemana.Sexta; //pegando o indice de sexta
+
+        Console.WriteLine(ds);
+      
+        
+    }
+}
