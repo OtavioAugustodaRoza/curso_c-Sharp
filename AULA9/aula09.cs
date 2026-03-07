8963b900e9bdbd2e35f6d0c3169505515690cf81
OtavioAugustodaRoza <otavioaugustodaroza@gmail.com>
2026-03-07 14:46:16 -0300

    FEAT: aula 9 finalizada

---
+++ AULA9/aula09.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Aula9
+{
+    static void Main()
+    {
+        int num=10;
+
+        // num=num<<1; dobra
+      //  num=num>>1; //metade
+        num=num>>2; //dobra 2 vezes 
+
+        Console.WriteLine(num);
+    }
+}
