51e334be855d5c398d1414f7c379845bd974f922
OtavioAugustodaRoza <otavioaugustodaroza@gmail.com>
2026-03-07 10:35:02 -0300

    FEAT: termino aula 7

---
+++ AULA7/aula07.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Aula7
+{
+    static void Main()
+    {
+        const string canal = "CFB  Cursos";
+        const double pi = 3.1415;
+
+        Console.WriteLine("Canal  {0,15} \n Pi {1,15}", canal,pi);
+        
+    }
+}
