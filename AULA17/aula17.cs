336384ec1b278471d2bb5567c6f37120ac66c841
OtavioAugustodaRoza <otavioaugustodaroza@gmail.com>
2026-03-08 10:59:17 -0300

    FEAT: aula17

---
+++ AULA17/aula17.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Aula17
+{
+    static void Main()
+    {
+        //int n1,n2,n3;
+        int[] n = new int[5];
+        //int[] num = new int[3]{55,77,99};
+        int[] num ={55,77,99};
+
+        n[0]=373;
+        n[1]=363;
+        n[2]=353;
+        n[3]=343;
+        n[4]=323;
+        
+        Console.WriteLine(num[1]);
+
+    }
+}
